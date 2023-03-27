999c489d2905da722820cbfe722b7ee09fc99b37
Selina Hernandez <selinah2002@utexas.edu>
2023-03-27 00:08:06 -0500

    Login/Registration Pages

---
+++ app/src/main/java/com/example/loginblueprint/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.loginblueprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView txt=findViewById(R.id.newuser);
+        txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this,Registration.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/loginblueprint/Registration.java
@@ -0,0 +1,26 @@
+package com.example.loginblueprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        final TextView txtr=findViewById(R.id.curruser);
+        txtr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Registration.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
